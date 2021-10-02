ad880b4654a65a0e3ca4074c3d9c6365586da870
EXTRAS12 <extra-kent@mail.ru>
2021-10-02 18:40:15 +0300

    part 3

--- electro/main/forms.py
+++ electro/main/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class CallForm(forms.Form):
-    subject = forms.CharField(label='Номер Вашего телефона', widget=forms.TextInput(attrs={'class': 'form-control'}))
-    content = forms.CharField(label='Сообщение', widget=forms.Textarea(attrs={'class': 'form-control', "rows": 5}))
+    subject = forms.CharField(label='Ваше имя', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    content = forms.CharField(label='Номер Вашего телефона', widget=forms.Textarea(attrs={'class': 'form-control', "rows": 3}))
